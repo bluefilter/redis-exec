08ec8c1baa8bb78818650dedd8bb39d24e02c6cc
Jonghwan Jeong <jonghwan@Jonghwanui-Macmini.local>
2024-12-18 22:12:32 +0900

    프로젝트 셋팅

---
+++ src/test/java/io/redispro/redisexec/RedisExecApplicationTests.java
@@ -0,0 +1,13 @@
+package io.redispro.redisexec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisExecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
